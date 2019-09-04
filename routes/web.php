33a59747b1e42252c125ce85c45c1bd0143e4381
haqqer <ilhamserbaguna@gmail.com>
2019-09-04 08:19:45 +0700

    Add mailer

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::get('/sendemail', 'MailController@index');
 Route::group([
   'as' => 'peserta',
   'prefix' => 'peserta'

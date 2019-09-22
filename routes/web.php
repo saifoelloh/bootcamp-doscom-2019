4cefeb31111fff17aff5f2ac2ab7d774c3e07897
saifoelloh <saifoelloh@gmail.com>
2019-09-22 19:09:37 +0700

    fixing auth issue

--- routes/web.php
+++ routes/web.php
@@ -20,40 +20,43 @@ Auth::routes();
 
 Route::post('peserta', 'PesertaController@store')->name('peserta.store');
 Route::get('/sendemail', 'MailController@index');
-Route::get('/home', 'HomeController@index')->name('home');
 Route::group([
-  'as' => 'peserta',
-  'prefix' => 'peserta',
-  'middleware' => 'auth:web',
-], function () {
-  Route::get('/', 'PesertaController@index');
-  Route::get('/create', 'PesertaController@create')->name('.create');
-  Route::get('/{id}', 'PesertaController@show')->name('.show');
-  Route::put('/{id}', 'PesertaController@update')->name('.update');
-  Route::put('/{id}/verify', 'PesertaController@verify')->name('.verify');
-  Route::delete('/{id}', 'PesertaController@destroy')->name('.destroy');
-});
+  'middleware' => 'auth:web'
+], function() {
+  Route::get('/home', 'HomeController@index')->name('home');
+  Route::group([
+    'as' => 'peserta',
+    'prefix' => 'peserta',
+  ], function () {
+    Route::get('/', 'PesertaController@index');
+    Route::get('/create', 'PesertaController@create')->name('.create');
+    Route::get('/{id}', 'PesertaController@show')->name('.show');
+    Route::put('/{id}', 'PesertaController@update')->name('.update');
+    Route::put('/{id}/verify', 'PesertaController@verify')->name('.verify');
+    Route::delete('/{id}', 'PesertaController@destroy')->name('.destroy');
+  });
 
-Route::group([
-  'as' => 'kelompok',
-  'prefix' => 'kelompok'
-], function () {
-  Route::get('/', 'KelompokController@index');
-  Route::post('/', 'KelompokController@store')->name('.store');
-  Route::get('/create', 'KelompokController@create')->name('.create');
-  Route::get('/{id}', 'KelompokController@show')->name('.show');
-  Route::put('/{id}', 'KelompokController@update')->name('.update');
-  Route::delete('/{id}', 'KelompokController@destroy')->name('.destroy');
-});
+  Route::group([
+    'as' => 'kelompok',
+    'prefix' => 'kelompok'
+  ], function () {
+    Route::get('/', 'KelompokController@index');
+    Route::post('/', 'KelompokController@store')->name('.store');
+    Route::get('/create', 'KelompokController@create')->name('.create');
+    Route::get('/{id}', 'KelompokController@show')->name('.show');
+    Route::put('/{id}', 'KelompokController@update')->name('.update');
+    Route::delete('/{id}', 'KelompokController@destroy')->name('.destroy');
+  });
 
-Route::group([
-  'as' => 'admin',
-  'prefix' => 'admin'
-], function () {
-  Route::get('/', 'AdminController@index');
-  Route::post('/', 'AdminController@store')->name('.store');
-  Route::get('/create', 'AdminController@create')->name('.create');
-  Route::get('/{id}', 'AdminController@show')->name('.show');
-  Route::put('/{id}', 'AdminController@update')->name('.update');
-  Route::delete('/{id}', 'AdminController@destroy')->name('.destroy');
+  Route::group([
+    'as' => 'admin',
+    'prefix' => 'admin'
+  ], function () {
+    Route::get('/', 'AdminController@index');
+    Route::post('/', 'AdminController@store')->name('.store');
+    Route::get('/create', 'AdminController@create')->name('.create');
+    Route::get('/{id}', 'AdminController@show')->name('.show');
+    Route::put('/{id}', 'AdminController@update')->name('.update');
+    Route::delete('/{id}', 'AdminController@destroy')->name('.destroy');
+  });
 });
